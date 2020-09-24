e5bafedec6960c95eaf7ea3cf63db5eee1aa572f
mirmidion43 <69776783+mirmidion43@users.noreply.github.com>
2020-09-24 12:53:56 +1000

    50% done

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Vector3 movement;
+    private float movementSqrMagnitude;
+    // Update is called once per frame
+    void Update()
+    {
+        GetMovementInput();
+        CharacterPosition();
+        CharacterRotation();
+        WalkingAnimation();
+        FootstepAudio();    
+    }
+
+    private void GetMovementInput()
+    {
+
+    }
+
+    private void CharacterPosition()
+    {
+
+    }
+    
+    private void CharacterRotation()
+    {
+
+    }
+
+    private void WalkingAnimation()
+    {
+
+    }
+    private void FootstepAudio()
+    {
+
+    }
+}
